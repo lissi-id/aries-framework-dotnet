6a7b22c5c86056412c0c2889a0e73e49fe63eb40
kenkosmowski <ken.kosmowski@gmx.de>
2023-09-20 12:12:57 +0200

    define VP & Pex interfaces and models

---
+++ src/Hyperledger.Aries/Features/OpenID4VP/Models/AuthorizationRequest.cs
@@ -0,0 +1,8 @@
+using Hyperledger.Aries.Features.Pex.Models;
+
+namespace Hyperledger.Aries.Features.OpenID4VP.Models;
+
+public class AuthorizationRequest
+{
+    public PresentationDefinition PresentationDefinition { get; set; }
+}
