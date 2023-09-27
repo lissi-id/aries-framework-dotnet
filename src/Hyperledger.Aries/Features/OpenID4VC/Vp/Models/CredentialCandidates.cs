cae784e16746c34eb293339da733a09629903a21
Kevin Dinh <kevin.dinh@neosfer.com>
2023-09-27 14:54:28 +0200

    add group property to CredentialCandidates
    Signed-off-by: Kevin Dinh <kevin.dinh@neosfer.com>
    Signed-off-by: kenkosmowski <ken.kosmowski@gmx.de>

--- src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/CredentialCandidates.cs
+++ src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/CredentialCandidates.cs
@@ -7,6 +7,8 @@ namespace Hyperledger.Aries.Features.OpenId4Vc.Vp.Models
     {
         public string InputDescriptorId { get; set; }
         
+        public string[] Group { get; set; }
+        
         public List<ICredential> Credentials { get; set; } = new List<ICredential>();
     }
 }
