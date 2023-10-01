b35f6606b5cffb31d931b99057a21dd1c9de03e8
Kevin Dinh <kevin.dinh@neosfer.com>
2023-10-01 18:05:41 +0200

    add function for creating sd-jwt presentation
    Signed-off-by: Kevin Dinh <kevin.dinh@neosfer.com>

--- src/Hyperledger.Aries/Features/SdJwt/Services/SdJwtVcHolderService/DefaultSdJwtVcHolderService.cs
+++ src/Hyperledger.Aries/Features/SdJwt/Services/SdJwtVcHolderService/DefaultSdJwtVcHolderService.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Agents;
+using Hyperledger.Aries.Features.OpenId4Vc.KeyStore.Services;
 using Hyperledger.Aries.Features.OpenId4Vc.Vci.Models.Metadata.Issuer;
 using Hyperledger.Aries.Features.OpenId4Vc.Vp.Models;
 using Hyperledger.Aries.Features.Pex.Models;
 using Hyperledger.Aries.Features.SdJwt.Models.Records;
 using Hyperledger.Aries.Storage;
-using Hyperledger.Aries.Storage.Models.Interfaces;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SD_JWT.Abstractions;
+using SD_JWT.Models;
 
 namespace Hyperledger.Aries.Features.SdJwt.Services.SdJwtVcHolderService
 {
@@ -24,6 +25,11 @@ namespace Hyperledger.Aries.Features.SdJwt.Services.SdJwtVcHolderService
         protected readonly IHolder Holder;
 
         /// <summary>
+        ///     The key store responsible for key operations.
+        /// </summary>
+        protected readonly IKeyStore KeyStore;
+
+        /// <summary>
         ///     The service responsible for wallet record operations.
         /// </summary>
         protected readonly IWalletRecordService RecordService;
@@ -31,30 +37,50 @@ namespace Hyperledger.Aries.Features.SdJwt.Services.SdJwtVcHolderService
         /// <summary>
         ///     Initializes a new instance of the <see cref="DefaultSdJwtVcHolderService" /> class.
         /// </summary>
+        /// <param name="keyStore">The key store responsible for key operations.</param>
         /// <param name="recordService">The service responsible for wallet record operations.</param>
         /// <param name="holder">The service responsible for holder operations.</param>
         public DefaultSdJwtVcHolderService(
             IHolder holder,
+            IKeyStore keyStore,
             IWalletRecordService recordService)
         {
             Holder = holder;
+            KeyStore = keyStore;
             RecordService = recordService;
+        }
+
+        /// <inheritdoc />
+        public async Task<string> CreatePresentation(SdJwtRecord credential, string[] disclosureNames,
+            string? audience = null,
+            string? nonce = null)
+        {
+            var disclosureNamesDict = new Dictionary<int, string>();
+            for (var i = 0; i < credential.Disclosures.Length; i++)
+            {
+                var disclosureName = Disclosure.Deserialize(credential.Disclosures[i]).Name;
+                disclosureNamesDict.Add(i, disclosureName);
+            }
+
+            var disclosures = disclosureNames
+                .Select(disclosureName => disclosureNamesDict.FirstOrDefault(x => x.Value == disclosureName).Key)
+                .Select(index => Disclosure.Deserialize(credential.Disclosures[index])).ToArray();
+
+            string? keybindingJwt = null;
+            if (!string.IsNullOrEmpty(credential.KeyId) && !string.IsNullOrEmpty(nonce) &&
+                !string.IsNullOrEmpty(audience))
+            {
+                keybindingJwt =
+                    await KeyStore.GenerateProofOfPossessionAsync(credential.KeyId, audience, nonce, "kb+jwt");
+            }
+
+            return Holder.CreatePresentation(credential.EncodedIssuerSignedJwt, disclosures, keybindingJwt);
         }
 
         /// <inheritdoc />
         public virtual async Task<bool> DeleteAsync(IAgentContext context, string recordId)
         {
             return await RecordService.DeleteAsync<SdJwtRecord>(context.Wallet, recordId);
-        }
-
-        /// <inheritdoc />
-        public virtual async Task<SdJwtRecord> GetAsync(IAgentContext context, string credentialId)
-        {
-            var record = await RecordService.GetAsync<SdJwtRecord>(context.Wallet, credentialId);
-            if (record == null)
-                throw new AriesFrameworkException(ErrorCode.RecordNotFound, "SD-JWT Credential record not found");
-
-            return record;
         }
 
         /// <inheritdoc />
@@ -71,7 +97,7 @@ namespace Hyperledger.Aries.Features.SdJwt.Services.SdJwtVcHolderService
                     throw new NotSupportedException("Only vc+sd-jwt format is supported");
                 }
 
-                if (inputDescriptor.Constraints.Fields == null  || inputDescriptor.Constraints.Fields.Length == 0)
+                if (inputDescriptor.Constraints.Fields == null || inputDescriptor.Constraints.Fields.Length == 0)
                 {
                     throw new InvalidOperationException("Fields cannot be null or empty");
                 }
@@ -84,14 +110,24 @@ namespace Hyperledger.Aries.Features.SdJwt.Services.SdJwtVcHolderService
                 }
 
                 var limitDisclosuresRequired = string.Equals(inputDescriptor.Constraints.LimitDisclosure, "required");
-                
+
                 var credentialCandidates = new CredentialCandidates(inputDescriptor.Id,
                     matchingCredentials, limitDisclosuresRequired);
-                
+
                 result.Add(credentialCandidates);
             }
 
             return Task.FromResult(result.ToArray());
+        }
+
+        /// <inheritdoc />
+        public virtual async Task<SdJwtRecord> GetAsync(IAgentContext context, string credentialId)
+        {
+            var record = await RecordService.GetAsync<SdJwtRecord>(context.Wallet, credentialId);
+            if (record == null)
+                throw new AriesFrameworkException(ErrorCode.RecordNotFound, "SD-JWT Credential record not found");
+
+            return record;
         }
 
         /// <inheritdoc />
