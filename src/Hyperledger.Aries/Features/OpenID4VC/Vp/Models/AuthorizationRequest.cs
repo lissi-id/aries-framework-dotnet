22569cf77c3cc7e0c7ed1788a84a20d24173999a
Johannes Tuerk <johannes.tuerk@main-incubator.com>
2023-10-04 12:57:14 +0100

    init
    Signed-off-by: Johannes Tuerk <johannes.tuerk@main-incubator.com>

--- src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/AuthorizationRequest.cs
+++ src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/AuthorizationRequest.cs
@@ -1,9 +1,40 @@
 using Hyperledger.Aries.Features.Pex.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Hyperledger.Aries.Features.OpenId4Vc.Vp.Models
 {
     public class AuthorizationRequest
     {
+        [JsonProperty("response_type")]
+        public string ResponseType { get; set; }
+        
+        [JsonProperty("client_id")]
+        public string ClientId { get; set; }
+        
+        [JsonProperty("client_id_scheme")]
+        public string ClientIdScheme { get; set; }
+        
+        [JsonProperty("redirect_uri")]
+        public string RedirectUri { get; set; }
+        
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+        
+        [JsonProperty("presentation_definition")]
+        public JObject PresentationDefinitionAsString { get; set; }
+        
+        [JsonProperty("nonce")]
+        public string Nonce { get; set; }
+
+        [JsonProperty("response_mode")]
+        public string ResponseMode { get; set; }
+        
+        [JsonProperty("state")]
+        public string State { get; set; }
+        
+        [JsonIgnore]
         public PresentationDefinition PresentationDefinition { get; set; }
+
     }
 }
