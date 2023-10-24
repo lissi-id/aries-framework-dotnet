5af314e7f5ffc5ce0e018f9bb45523ff94ce4afd
Johannes Tuerk <johannes.tuerk@main-incubator.com>
2023-10-24 15:35:05 +0100

    doc comments
    Signed-off-by: Johannes Tuerk <johannes.tuerk@main-incubator.com>

--- src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/AuthorizationRequest.cs
+++ src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/AuthorizationRequest.cs
@@ -7,41 +7,80 @@ using Newtonsoft.Json;
 
 namespace Hyperledger.Aries.Features.OpenId4Vc.Vp.Models
 {
+    /// <summary>
+    ///     Represents the Request of a Verifier to a Holder within the OpenId4VP specification.
+    /// </summary>
     public class AuthorizationRequest
     {
+        /// <summary>
+        ///     Gets or sets the response type. Determines what the Authorization Response should contain.
+        /// </summary>
         [JsonProperty("response_type")] 
         public string ResponseType { get; set; } = null!;
 
+        /// <summary>
+        ///     Gets or sets the client id. The Identifier of the Verifier
+        /// </summary>
         [JsonProperty("client_id")] 
         public string ClientId { get; set; } = null!;
 
+        /// <summary>
+        ///     Gets or sets the client id scheme.
+        /// </summary>
         [JsonProperty("client_id_scheme")] 
         public string? ClientIdScheme { get; set; }
 
+        /// <summary>
+        ///    Gets or sets the redirect uri.
+        /// </summary>
         [JsonProperty("redirect_uri")] 
         public string? RedirectUri { get; set; }
         
+        /// <summary>
+        ///     Gets or sets the client metadata. Contains the Verifier metadata
+        /// </summary>
         [JsonProperty("client_metadata")] 
         public string? ClientMetadata { get; set; }
 
+        /// <summary>
+        ///    Gets or sets the client metadata uri. Can be used to retrieve the verifier metadata.
+        /// </summary>
         [JsonProperty("client_metadata_uri")] 
         public string? ClientMetadataUri { get; set; }
 
+        /// <summary>
+        ///    The scope of the request.
+        /// </summary>
         [JsonProperty("scope")] 
         public string? Scope { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the nonce. Random string for session binding.
+        /// </summary>
         [JsonProperty("nonce")] 
         public string Nonce { get; set; } = null!;
 
+        /// <summary>
+        ///     Gets or sets the response mode. Determines how to send the Authorization Response.
+        /// </summary>
         [JsonProperty("response_mode")] 
         public string? ResponseMode { get; set; }
         
+        /// <summary>
+        ///  Gets or sets the response mode. Determines where to send the Authorization Response to.
+        /// </summary>
         [JsonProperty("response_uri")] 
         public string? ResponseUri { get; set; }
 
+        /// <summary>
+        ///    Gets or sets the state.
+        /// </summary>
         [JsonProperty("state")] 
         public string? State { get; set; }
 
+        /// <summary>
+        ///    Gets or sets the presentation definition. Contains the claims that the Verifier wants to receive.
+        /// </summary>
         [JsonIgnore]
         public PresentationDefinition PresentationDefinition { get; set; } = null!;
 
--- src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/PresentedCredential.cs
+++ src/Hyperledger.Aries/Features/OpenID4VC/Vp/Models/PresentedCredential.cs
@@ -2,10 +2,19 @@ using System.Collections.Generic;
 
 namespace Hyperledger.Aries.Features.OpenId4Vc.Vp.Models
 {
+    /// <summary>
+    ///     Represents a presented credential.
+    /// </summary>
     public class PresentedCredential
     {
+        /// <summary>
+        ///     Gets or Sets the claims of the credential that were presented.
+        /// </summary>
         public Dictionary<string, string> PresentedClaims { get; set; } = null!;
         
+        /// <summary>
+        ///     Gets or Sets the credential id.
+        /// </summary>
         public string CredentialId { get; set; } = null!;
     }
 }
